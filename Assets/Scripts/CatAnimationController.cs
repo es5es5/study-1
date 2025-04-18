f733f59fe657938bea08bbf678289d60ad667622
es5es5 <es5e5s@nate.com>
2025-04-19 00:16:57 +0900

    고양이 R D 키 입력시 동작

---
+++ Assets/Scripts/CatAnimationController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class CatAnimationController : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            animator.SetTrigger("Run");
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            animator.SetTrigger("Dead");
+        }
+    }
+
+    // ✅ UI 버튼 연결용 함수
+    public void PlayRun()
+    {
+        animator.SetTrigger("Run");
+    }
+
+    public void PlayDead()
+    {
+        animator.SetTrigger("Dead");
+    }
+}
